4c5234a7532d6e1573f92a08111a6643560282fb
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-03-16 15:09:28 +0700

    Create Tugas_3.java

---
+++ minggu 2/Tugas/Tugas 3/Tugas_3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_3;
+
+/**
+ *
+ * @author denic
+ */
+public class Tugas_3 {
+
+    boolean gameOver = false;
+    //tidak ada kesalahan dalam pendeklarasian variabel//
+    int student=50,classes=3;
+    //tidak ada kesalahan dalam pendeklarasian variabel//
+    double sales_tax;
+    //tidak ada kesalahan dalam pendeklarasian variabel//
+    short number1;
+    //tidak ada kesalahan dalam pendeklarasian variabel//
+    
+    int 2beOrNot2be;
+    //kesalahan pendeklarasian variabel ini adalah karena nama variabel yang digunakan diawali oleh angka atau bilangan//
+    float price index;
+    //kesalahan pendeklarasian variabel ini adalah karena nama variabel yang digunakan tidak boleh menggunakan spasi//
+    double lastYears'Price';
+    //kesalahan pendeklarasian variabel ini adalah karena nama variabel yang digunakan tidak boleh menggunakan simbol ditengah nama kecuali underscore(_)//
+    long class;
+    //kesalahan pendeklarasian variabel ini adalah karena nama variabel yang digunakan merupakan reserved word atau keyword java yang tertanam dalam program java// 
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
