12fc5896c024b91d173619e894488b76940c1bb9
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-03-16 15:06:56 +0700

    Create latihan_a.java

---
+++ minggu 2/Latihan/latihan_a.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author denic
+ */
+public class latihan_a {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello");
+        System.out.println("World !");
+        
+        
+        System.out.print("Hello");
+        System.out.print("World !");
+    }
+    
+}
