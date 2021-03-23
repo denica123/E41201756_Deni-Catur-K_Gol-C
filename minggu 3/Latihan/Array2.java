9284036c27f1c753a81a01468af1a258db5499d1
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-03-23 12:49:27 +0700

    Create Array2.java

---
+++ minggu 3/Latihan/Array2.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_array1;
+
+/**
+ *
+ * @author denic
+ */
+public class Array2 {
+    
+    public static void main(String[] args) {
+        
+        int[] angka = {5, 10, 17, 20, 1};
+        String[] teks = {"Saya","sedang","belajar","OOP","Java"};
+        
+        for(int i=0; i<angka.length; i++){
+            System.out.println(angka[i]);
+        }
+        
+        for(int j=0; j<teks.length; j++){
+            System.out.print(teks[j]);
+            System.out.print(" ");
+    
+        }
+    }
+ 
+}
