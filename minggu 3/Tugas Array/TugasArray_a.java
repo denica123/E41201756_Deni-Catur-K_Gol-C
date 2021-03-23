77e6657460de34506cc818f1ad8f91430a15d36e
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-03-23 12:51:22 +0700

    Create TugasArray_a.java

---
+++ minggu 3/Tugas Array/TugasArray_a.java
@@ -0,0 +1,23 @@
+
+package tugasarray;
+
+/**
+ *
+ * @author denic
+ */
+public class TugasArray_a {
+
+   
+    public static void main(String[] args) {
+        
+       String[] teks = {"Deni","Catur","Kusumawati"};
+       
+       
+       for(int i=0; i<teks.length; i++){
+            System.out.print(teks[i]);
+            System.out.print(" ");
+    
+        }
+    }
+    
+}
