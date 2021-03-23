6526a3497acf83a699865a74058647198b2f5f09
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-03-23 12:50:52 +0700

    Create Latihan_Array1.java

---
+++ minggu 3/Latihan/Latihan_Array1.java
@@ -0,0 +1,18 @@
+
+package latihan_array1;
+
+/**
+ *
+ * @author denic
+ */
+public class Latihan_Array1 {
+
+    
+    public static void main(String[] args) {
+        int[] angka = {3, 7, 8, 1, 10, 20, 35};
+        
+        System.out.println("jumlah elemen array angka = " +angka.length);
+       
+    }
+    
+}
