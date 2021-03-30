fcdaa8e14c18ff45e2d41398a394d3f5ddf528c7
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-03-30 12:13:18 +0700

    Create ControlStatement.java

---
+++ minggu 4/Latihan/ControlStatement.java
@@ -0,0 +1,23 @@
+
+package control.statement;
+
+/**
+ *
+ * @author denic
+ */
+public class ControlStatement {
+
+    
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while (i <= 10){
+            //blok yang akan berulang
+            System.out.println("Perulangan ke-" + i);
+            
+    //increment nilai i
+    i++;
+        }
+    }
+    
+}
