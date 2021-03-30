487a174386a509c57a42eec6095f63ec0bb0335b
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-03-30 12:14:05 +0700

    Create Statement_for_each.java

---
+++ minggu 4/Latihan/Statement_for_each.java
@@ -0,0 +1,18 @@
+
+package control.statement;
+
+/**
+ *
+ * @author denic
+ */
+public class Statement_for_each {
+    public static void main (String[] args){
+        //membuat array
+        int angka[] = {3,1,42,24,12};
+        
+        //menggunakan perulangan for each untuk menampilkan angka
+        for (int x : angka ){
+            System.out.print( x + " " );
+        }
+    }
+}
