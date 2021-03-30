fd625a70b0b09a7162a4171ba72ea6b05ad73c49
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-03-30 12:13:51 +0700

    Create Statement_for.java

---
+++ minggu 4/Latihan/Statement_for.java
@@ -0,0 +1,21 @@
+package control.statement;
+
+/**
+ *
+ * @author denic
+ */
+public class Statement_for {
+    
+    public static void main(String[] args){
+        
+        for (int i = 0; i <= 5; i++){
+            
+            for (int j = 0; j <= i; j++){
+                
+                System.out.print(" * ");
+            }
+            System.out.println(" ");
+        }
+    }
+    
+}
