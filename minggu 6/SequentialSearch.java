37017dceab80908ae1727415cf85f28f2ae352fd
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-18 12:31:52 +0700

    Create SequentialSearch.java

---
+++ minggu 6/SequentialSearch.java
@@ -0,0 +1,33 @@
+
+package minggu_6;
+
+
+public class SequentialSearch {
+    public static void main(String[] args){
+        int[] exampleVariabelOne = {2, 9, 6, 7, 4, 5, 3, 0, 1};
+        int target = 4;
+        sequentialSearch(exampleVariabelOne, target);
+    }
+    
+    public static void sequentialSearch (int[] parameterOne, int parameterTwo){
+        int index = -1;
+        //searches each index of the array unil it reaches the last index 
+        
+        for (int i = 0; i < parameterOne.length; i++){
+            if (parameterOne[i] == parameterTwo){
+                //if the target is found, int index is set as the value of i and 
+                // the for loop is terminated 
+                
+                index = i;
+                break;
+            }
+        }
+        if (index == -1){
+            System.out.println("Your target integer does not exist in the array");
+        } else {
+            System.out.println("Your target integer is in index " + index + " of the array");
+        }
+  
+    }
+    
+}
