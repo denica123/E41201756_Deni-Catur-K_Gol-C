3287af57980a27940af72a04fb2e2e4000f3d195
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-23 21:02:54 +0700

    Create Demo.java

---
+++ minggu 7/Latihan/pkgabstract/Demo.java
@@ -0,0 +1,14 @@
+
+package pkgabstract;
+
+/**
+ *
+ * @author denic
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B ob = new B();
+        ob.method1();
+        ob.method2();
+    }
+}
