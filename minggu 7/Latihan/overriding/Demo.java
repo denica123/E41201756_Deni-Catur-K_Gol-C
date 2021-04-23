bc35f3b7789f0d7c79af6f158dae29cbd481dbaa
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-23 20:59:44 +0700

    Create Demo.java

---
+++ minggu 7/Latihan/overriding/Demo.java
@@ -0,0 +1,13 @@
+
+package overriding;
+
+/**
+ *
+ * @author denic
+ */
+public class Demo {
+    public static void main(String[] args) {
+        B subOb = new B();
+        subOb.tampilkanKeLayar();
+    }
+}
