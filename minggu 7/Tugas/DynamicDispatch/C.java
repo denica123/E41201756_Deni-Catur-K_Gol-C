0f9587f52e9a368c6c1e89ee33f7fa953e7db66d
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-24 00:11:30 +0700

    Create C.java

---
+++ minggu 7/Tugas/DynamicDispatch/C.java
@@ -0,0 +1,12 @@
+
+package minggu7.DynamicDispatch;
+
+/**
+ *
+ * @author denic
+ */
+public class C extends A {
+    void callthis(){
+        System.out.println("Inside Class C's Method!");
+    }
+}
