0c44e6270c495c62ebc768f71c133fa7cf334670
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-23 21:00:02 +0700

    Create A.java

---
+++ minggu 7/Latihan/pewarisan/A.java
@@ -0,0 +1,18 @@
+
+package pewarisan;
+
+/**
+ *
+ * @author denic
+ */
+
+public class A {
+    int x;
+    int y;
+
+    void TampilkanNilaiXY() {
+        System.out.println("Nilai X : " + x + ", y : " + y);
+    }
+}
+    
+
