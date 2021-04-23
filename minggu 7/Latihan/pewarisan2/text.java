26a4d62d47f16e88af90b87a54d89f38ff298faf
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-23 21:02:01 +0700

    Create text.java

---
+++ minggu 7/Latihan/pewarisan2/text.java
@@ -0,0 +1,13 @@
+
+package pewarisan2;
+
+/**
+ *
+ * @author denic
+ */
+public class text {
+    public static void main(String[] args) {
+        Pelajar mahasiswa = new Pelajar("DENI", 165, 50, "E41201756", "SMKN 1 Nganjuk", 95);
+        System.out.println(mahasiswa.toString());
+    }
+}
