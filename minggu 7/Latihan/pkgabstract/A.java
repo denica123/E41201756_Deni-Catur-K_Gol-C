311b8cc272e655e724d1a5d5097c05c4360e72de
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-23 21:02:20 +0700

    Create A.java

---
+++ minggu 7/Latihan/pkgabstract/A.java
@@ -0,0 +1,15 @@
+
+package pkgabstract;
+
+/**
+ *
+ * @author denic
+ */
+public abstract class A { // Deklarasi Class
+    abstract public void method2();
+
+    public void method1(){
+        System.out.println("Method konkrit dari class A");
+    }
+    
+}
