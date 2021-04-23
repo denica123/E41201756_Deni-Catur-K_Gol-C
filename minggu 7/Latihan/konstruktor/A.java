5a2dc19ada4154dc908ae2d2aad4c80af128ebfc
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-23 20:58:02 +0700

    Create A.java

---
+++ minggu 7/Latihan/konstruktor/A.java
@@ -0,0 +1,15 @@
+
+package konstruktor;
+
+/**
+ *
+ * @author denic
+ */
+public class A {
+    A(String param1){
+        System.out.println(" Konstruktor class A dengan Parameter " + param1 + "dieksekusi....");
+    }
+
+    
+    
+}
