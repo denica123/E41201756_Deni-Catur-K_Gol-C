aca95ca28d30b2d709e9ca5274a6f83ef7b0c552
Deni Catur Kusumawati <75110391+denica123@users.noreply.github.com>
2021-04-23 20:58:23 +0700

    Create B.java

---
+++ minggu 7/Latihan/konstruktor/B.java
@@ -0,0 +1,13 @@
+
+package konstruktor;
+
+/**
+ *
+ * @author denic
+ */
+public class B extends A {
+    B(){
+        super ("test"); //tambahan
+        System.out.println(" Konstruktor class B dieksekusi....");
+    }
+}
